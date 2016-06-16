2cf59ff7cd8965126efd121903098f397a26efae
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-06-16 08:39:18 +0000

    modificato footer

--- footer.php
+++ footer.php
@@ -12,18 +12,17 @@
 
 		</div><!-- .site-content -->
 
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php if ( has_nav_menu( 'primary' ) ) : ?>
-				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'primary',
-							'menu_class'     => 'primary-menu',
-						 ) );
-					?>
-				</nav><!-- .main-navigation -->
-			<?php endif; ?>
+		<footer id="colophon" class="site-footer full-width" role="contentinfo">
 
+			<div class="site-info content-wrapper">
+				<span class="site-title"><a class="screen-reader-text" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+			</div><!-- .site-info -->
+			
+			<div class="site-info content-wrapper">
+				<p><?php bloginfo('contact_address'); ?> <?php bloginfo('contact_address2'); ?><br /><a href="tel:<?php bloginfo('contact_phone'); ?>"><?php bloginfo('contact_phone'); ?></a><br/><?php echo hide_email(get_bloginfo('contact_email','display')); ?></a></p>
+				<p><?php bloginfo('contact_company_name'); ?><br /><?php _e('P.IVA','studiopanebarco'); ?>&nbsp;<?php bloginfo('contact_vat'); ?></p>
+			</div><!-- .site-info -->
+			
 			<?php if ( has_nav_menu( 'social' ) ) : ?>
 				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
 					<?php
@@ -37,19 +36,19 @@
 					?>
 				</nav><!-- .social-navigation -->
 			<?php endif; ?>
-
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+			
+			<?php if ( has_nav_menu( 'footer' ) ) : ?>
+				<nav class="service-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Legal Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footer',
+							'menu_class'     => 'footer-menu',
+						 ) );
+					?>
+				</nav><!-- .main-navigation -->
+			<?php endif; ?>			
+			
+			<div id="credits">Made by <a href="http://www.silverbackstudio.it" target="_blank" rel="external">SilverbackStudio</a></div>
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
 </div><!-- .site -->
