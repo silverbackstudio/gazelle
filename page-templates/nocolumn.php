9e84951b9b05a4e1266efc85dca906edfbb10648
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-06-17 14:45:49 +0000

    added page templates

---
+++ page-templates/nocolumn.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: No Columns
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_template_part('page');
