12ffc8c53c91197a90a8580094f9bae777e7972d
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-06-21 14:16:00 +0000

    fix band file naming

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -404,7 +404,7 @@ function has_instagram(){
 function theme_content_bands($field_name='content_band') {
 	if( function_exists('have_rows') ):
 		while ( have_rows($field_name) ) : the_row(); 
-				get_template_part('template-parts/bands', preg_replace("/[^a-zA-Z0-9_-]+/", "", get_row_layout()) );
+				get_template_part('template-parts/band', preg_replace("/[^a-zA-Z0-9_-]+/", "", get_row_layout()) );
 		endwhile; 
 	endif;
 }
