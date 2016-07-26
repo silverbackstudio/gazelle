70063bcc62395adf9eb1c98f5879204cb99fe108
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-07-26 14:31:52 +0000

    set header image to correct size

--- header.php
+++ header.php
@@ -96,7 +96,7 @@
 					 * for Custom Header. Default '(max-width: 709px) 85vw,
 					 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
 					 */
-					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
+					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 1320px) 100vw, 1320px' );
 				?>
 				<div class="header-image image-fit-cover">
 						<img src="<?php header_image(); ?>" srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ); ?>" sizes="<?php echo esc_attr( $custom_header_sizes ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
