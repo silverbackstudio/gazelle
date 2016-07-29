6d12a53777c5043e41d1d8bbc10221db35ebbda0
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-07-29 09:30:40 +0000

    changed image sizes attriburte

--- functions.php
+++ functions.php
@@ -590,14 +590,6 @@ function content_image_sizes_attr( $sizes, $size ) {
 		
 		return $sizes;
 	}
-	840 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 62vw, 840px';
-
-	if ( 'page' === get_post_type() ) {
-		840 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
-	}else {
-		840 > $width && 600 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 61vw, (max-width: 1362px) 45vw, 600px';
-		600 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
-	}
 
 	return $sizes;
 }
@@ -615,15 +607,9 @@ add_filter( 'wp_calculate_image_sizes', 'content_image_sizes_attr', 10 , 2 );
  * @return string A source size value for use in a post thumbnail 'sizes' attribute.
  */
 function post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
-	/*
+
 	if ( 'post-thumbnail' === $size ) {
-		is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
-		! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
-	}
-	*/
-	
-	if ( 'post-thumbnail' === $size ) {
-		is_page() && $attr['sizes'] = '(max-width: 1320px) 100vw, 1320px';
+		$attr['sizes'] = '(max-width: 710px) 100vw, (max-width: 910px) 50vw, 430px';	
 	} else if( 'band' === $size) {
 		$attr['sizes'] = '(max-width: 910px) 100vw, (max-width: 1320px) 70vw,  880px';
 	} else if( 'band-full' === $size) {
